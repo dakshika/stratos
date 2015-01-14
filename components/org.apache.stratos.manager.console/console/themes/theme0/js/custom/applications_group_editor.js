8d605d5c24af458ce39c202f90928236ec4aa5af
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-01-14 17:01:52 +0530

    Group editor initial version completed

--- components/org.apache.stratos.manager.console/console/themes/theme0/js/custom/applications_group_editor.js
+++ components/org.apache.stratos.manager.console/console/themes/theme0/js/custom/applications_group_editor.js
@@ -80,7 +80,7 @@ var generatedGroupOptions = {
 
 jsPlumb.ready(function() {
     //create application level block
-    jsPlumb.addEndpoint('groupBase', {
+    jsPlumb.addEndpoint('group-base', {
         anchor:"BottomCenter"
     }, bottomConnectorOptions);
 });
@@ -169,11 +169,271 @@ function dagrePosition(){
     jsPlumb.repaintEverything();
 }
 
+function genGroupJSON(collector, connections){
+    //generate raw data tree from connections
+    var collector = collector || {};
+
+    var rawtree = [];
+
+    //get base-node data
+    var groupBase = $('#group-base').attr('data-generated');
+    if(groupBase){
+        var groupBaseJSON =  JSON.parse(decodeURIComponent(groupBase));
+        collector = groupBaseJSON;
+    }
+    //create empty nodes for group and cartridges
+    collector['groups']=[];
+    collector['cartridges']=[];
+
+    $.each(connections, function (idx, connection) {
+        var dataType = $('#'+connection.targetId).attr('data-type');
+        var dataGenerated  = $('#'+connection.targetId).attr('data-generated');
+        var jsonContent;
+        if(dataGenerated != undefined){
+            var jsonContent = JSON.parse(decodeURIComponent(dataGenerated));
+        }else{
+
+            var jsonContent = {cname: $('#'+connection.targetId).attr('data-ctype')};
+        }
+        rawtree.push({
+            parent: connection.sourceId,
+            content: jsonContent,
+            dtype: dataType,
+            id: connection.targetId
+        });
+
+    });
+
+    //generate heirache by adding json and extra info
+    var nodes = [];
+    var toplevelNodes = [];
+    var lookupList = {};
+
+    for (var i = 0; i < rawtree.length; i++) {
+        var n = rawtree[i].content;
+        if(rawtree[i].dtype == 'cartridge'){
+            n.id =  rawtree[i].id;
+            n.parent_id = ((rawtree[i].parent == 'group-base') ? 'group-base': rawtree[i].parent);
+            n.dtype =rawtree[i].dtype;
+        }else if(rawtree[i].dtype == 'group'){
+            n.id =  rawtree[i].id;
+            n.parent_id = ((rawtree[i].parent == 'group-base') ? 'group-base': rawtree[i].parent);
+            n.dtype =rawtree[i].dtype;
+            n.groups = [];
+            n.cartridges =[];
+        }
+
+        lookupList[n.id] = n;
+        nodes.push(n);
+
+        if (n.parent_id == 'group-base' && rawtree[i].dtype == 'cartridge') {
+            collector['cartridges'].push(n.cname);
+        }else if(n.parent_id == 'group-base' && rawtree[i].dtype == 'group'){
+            collector['groups'].push(n);
+        }
+
+    }
+
+    //merge any root level stuffs
+    for (var i = 0; i < nodes.length; i++) {
+        var n = nodes[i];
+        if (!(n.parent_id == 'group-base') && n.dtype == 'cartridge') {
+            lookupList[n.parent_id]['cartridges'] = lookupList[n.parent_id]['cartridges'].concat([n.cname]);
+        }else if(!(n.parent_id == 'group-base') && n.dtype == 'group'){
+            lookupList[n.parent_id]['groups'] = lookupList[n.parent_id]['groups'].concat([n]);
+        }
+    }
+
+    //cleanup JSON, remove extra items added to object level
+    function traverse(o) {
+        for (var i in o) {
+            if(i == 'id' || i == 'parent_id' || i == 'dtype'){
+                delete o[i];
+            }else if(i == 'groups' && o[i].length == 0){
+                delete o[i];
+            }
+            if (o[i] !== null && typeof(o[i])=="object") {
+                //going on step down in the object tree!!
+                traverse(o[i]);
+            }
+        }
+    }
+
+    traverse(collector);
+
+    return collector;
+
+}
+
+
+var groupBlockTemplate = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "id": "root",
+    "title":" ",
+    "type": "object",
+    "options": {
+        "disable_properties": true,
+        "disable_collapse": true
+    },
+    "properties": {
+        "name": {
+            "id": "root/name",
+            "type": "string",
+            "title": "Group Name: "
+        },
+        "isGroupScalingEnabled": {
+            "id": "root/isGroupScalingEnabled",
+            "type": "boolean",
+            "title": "Is Group Scaling Enabled: "
+        },
+        "dependencies": {
+            "id": "root/dependencies",
+            "type": "object",
+            "title": "Dependencies: ",
+            "options": {
+                "disable_properties": true
+            },
+            "properties": {
+                "startupOrders": {
+                    "id": "root/dependencies/startupOrders",
+                    "type": "array",
+                    "title": "Startup Orders: ",
+                    "items": {
+                        "id": "root/dependencies/startupOrders/0",
+                        "type": "string"
+                    }
+                },
+                "scalingDependants": {
+                    "id": "root/dependencies/scalingDependants",
+                    "type": "array",
+                    "title": "Scaling Dependants: ",
+                    "items": {
+                        "id": "root/dependencies/scalingDependants/0",
+                        "type": "string"
+                    }
+                },
+                "terminationBehaviour": {
+                    "id": "root/dependencies/terminationBehaviour",
+                    "type": "string",
+                    "title": "Termination Behaviour: ",
+                    "enum": ["terminate-none","terminate-dependents","terminate-all"]
+                },
+                "required": [
+                    "startupOrders",
+                    "scalingDependants",
+                    "terminationBehaviour"
+                ]
+            }
+        },
+        "required": [
+            "name",
+            "isGroupScalingEnabled",
+            "dependencies"
+        ]
+    }
+};
+
+var groupBlockDefault = {
+    "name": "",
+    "isGroupScalingEnabled": "true",
+    "dependencies": {
+        "startupOrders": [
+            "cartridge.type, group.name"
+        ],
+        "scalingDependants": [
+            "cartridge.type, group.name"
+        ],
+        "terminationBehaviour": "terminate-all"
+    }
+};
+
 // Document ready events
 $(document).ready(function(){
 
+    $('#deploy').on('click', function(){
+        var  payload = genGroupJSON({}, jsPlumb.getConnections());
+        var btn = $(this);
+        var formtype = 'groups';
+        btn.html("<i class='fa fa-spinner fa-spin'></i> Adding...");
+        $.ajax({
+            type: "POST",
+            url: caramel.context + "/controllers/applications/application_requests.jag",
+            dataType: 'json',
+            data: { "formPayload": JSON.stringify(payload), "formtype": formtype },
+            success: function (data) {
+                if (data.status == 'error') {
+                    var n = noty({text: data.message, layout: 'bottomRight', type: 'error'});
+                } else if (data.status == 'warning') {
+                    var n = noty({text: data.message, layout: 'bottomRight', type: 'warning'});
+                } else {
+                    var n = noty({text: data.message, layout: 'bottomRight', type: 'success'});
+                }
+            }
+        })
+            .always(function () {
+                btn.html('Add Group Definition');
+            });
+
+    });
+
+    //*******************Adding JSON editor *************//
+    JSONEditor.defaults.theme = 'bootstrap3';
+    JSONEditor.defaults.iconlib = 'fontawesome4';
+    JSONEditor.defaults.show_errors = "always";
+    var editor, blockId = 'group-base';
+
+    // Initialize the editor
+    editor = new JSONEditor(document.getElementById('general-data'), {
+        ajax: false,
+        disable_edit_json: true,
+        schema: groupBlockTemplate,
+        format: "grid",
+        startval: groupBlockDefault
+    });
+
     DragEl(".stepnode");
     Repaint();
+
+    function tabData(node){
+
+        blockId = node.attr('id');
+        var blockType = node.attr('data-type');
+        var startval;
+        if(blockType == 'group'){
+            if(node.attr('data-generated')) {
+                startval = JSON.parse(decodeURIComponent(node.attr('data-generated')));
+            }else{
+                startval = groupBlockDefault;
+            }
+
+            $('#general-data').html('');
+
+            // Initialize the editor
+            editor = new JSONEditor(document.getElementById('general-data'), {
+                ajax: false,
+                disable_edit_json: true,
+                schema: groupBlockTemplate,
+                format: "grid",
+                startval: startval
+            });
+        }
+
+    }
+
+    $('#whiteboard').on('click', '.stepnode', function(){
+        tabData($(this))
+    });
+
+    //get component JSON data
+    $('#component-info-update').on('click', function(){
+        $('#'+blockId).attr('data-generated', encodeURIComponent(JSON.stringify(editor.getValue())));
+        $('#'+blockId).removeClass('input-false');
+        $('#'+blockId).find('div>i').removeClass('fa-exclamation-circle').addClass('fa-check-circle-o').css('color','#2ecc71');
+        if(blockId == 'group-base'){
+            $('#deploy').prop("disabled", false);
+        }
+
+    });
 
     //get create cartridge list
     generateCartridges(cartridgeList);
@@ -215,5 +475,5 @@ $(document).ready(function(){
 
 //bootstrap tooltip added
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip();
 })
